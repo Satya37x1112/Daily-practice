d35d7d2a460b92897f77eb1e6d25d7a893ef9ae2
Satya Sarthak Manohari <121684661+Satya37x1112@users.noreply.github.com>
2025-07-11 11:34:03 +0530

    Add files via upload

---
+++ challenge1.py
@@ -0,0 +1,6 @@
+from tkinter import*
+s_root=Tk()
+s_root.geometry("500x400")
+title_label=Label(text="Ready",fg="Red",bg="Violet",relief=SUNKEN,font="Comicsansms 19 bold",padx=30,pady=33)
+title_label.pack(side=BOTTOM,fill=X)
+s_root.mainloop()
