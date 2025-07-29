481621947f69b22fcfa17261e7cb637049b4f0cc
Satya Sarthak Manohari <121684661+Satya37x1112@users.noreply.github.com>
2025-07-29 11:30:25 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+//Code :-
+package Basics;
+public class HelloWorld {
+byte byte1;
+short short1;
+int int1;
+long long1;
+float float1;
+double double1;
+char char1;
+boolean boolean1;
+public static void main(String[] args) {
+HelloWorld obj = new HelloWorld();
+System.out.println("Default byte value: " + obj.byte1);
+System.out.println("Default short value: " + obj.short1);
+System.out.println("Default int value: " + obj.int1);
+System.out.println("Default long value: " + obj.long1);
+System.out.println("Default float value: " + obj.float1);
+System.out.println("Default double value: " + obj.double1);
+System.out.println("Default char value: '" + obj.char1 + "'");
+System.out.println("Default boolean value: " + obj.boolean1);
+}
+}
+
+	
